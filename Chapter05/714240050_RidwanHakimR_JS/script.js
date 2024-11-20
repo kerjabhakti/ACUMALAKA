bfc9ed2e484409f1e81c895a8244f282c14c7fd6
911AGENT <ridwanhakimrmdhn@gmail.com>
2024-11-20 20:50:12 +0700

    perubahan

--- Chapter05/714240050_RidwanHakimR_JS/script.js
+++ Chapter05/714240050_RidwanHakimR_JS/script.js
@@ -38,6 +38,7 @@ function jika(){
 }
 
 // image
+
 function foto1(){
     var spider = document.getElementById('spiderman').src='img/spider2.jpg';
 }
@@ -46,6 +47,13 @@ function foto2(){
 }
 
 // paragraph
-function paragraph(){
-    var hasilparagraph =  document.getElementById('hasilparagraph').innerHTML = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium aliquid ad nesciunt aperiam fugiat ipsum itaque exercitationem, eligendi officia libero reiciendis quisquam nostrum tempora fuga veritatis! Magni culpa, ex nisi a, reiciendis obcaecati excepturi minus alias id harum magnam unde explicabo recusandae accusantium ab quos accusamus quo vel atque hic.";
+    const hasilparagraph = ["", "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium aliquid ad nesciunt aperiam fugiat ipsum itaque exercitationem, eligendi officia libero reiciendis quisquam nostrum tempora fuga veritatis! Magni culpa, ex nisi a, reiciendis obcaecati excepturi minus alias id harum magnam unde explicabo recusandae accusantium ab quos accusamus quo vel atque hic."];
+
+    let currrentIndex = 0;
+
+    function paragraph(){
+        //update index ke paragraf berikutnya
+        currrentIndex = (currrentIndex + 1) % hasilparagraph.length;
+        // ganti teks paragraf
+        document.getElementById("hasilpg").textContent = hasilparagraph[currrentIndex];
 }
