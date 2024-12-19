84f7cb26ae443a47e8cda78b8539eaca387a3c2e
Nuthfih <asatriaraghib@gmail.com>
2024-12-19 15:12:22 +0700

    ini tugas praktikum 8

---
+++ Chapter08/AlifSatriaRaghib_714240005/boolean.php
@@ -0,0 +1,8 @@
+<?php 
+    $test=true;
+    if($test=true){
+        echo "Nilai data boolean TRUE";
+    } else{
+        echo "Nilai data boolean FALSE";
+    }
+?>
---
+++ Chapter08/AlifSatriaRaghib_714240005/fungsi.php
@@ -0,0 +1,5 @@
+<?
+function namaFungsi(){
+    //blok program
+}
+?>
