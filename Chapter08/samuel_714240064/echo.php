1a75bd2b70efbf6e9561c22d549b96423c4d8620
SamuelSidabutar <SamuelSidabutar>
2024-12-19 15:56:19 +0700

    tugasphpsamuel

---
+++ Chapter08/samuel_714240064/echo.php
@@ -0,0 +1,5 @@
+<?php
+    echo "BELAJAR PHP DI POLTEKPOS!<BR> \n";
+    echo "HALLO WORD!<BR>\n";
+    ECHO "AKU SEDANG BELAJAR PHP!<BR> \n"; 
+    echo "INI TEKS YANG DIBUAT TERPISAH!<BR> \n";
---
+++ Chapter08/samuel_714240064/perbandingan.php
@@ -0,0 +1,10 @@
+<?php
+    $a = 100;
+    $b = 20;
+    
+    printf ("$b > $b =%d <BR> \n", $a > $b);
+    printf ("$b >= $b =%d <BR> \n", $a >= $b);
+    printf ("$b < $b =%d <BR> \n", $a < $b);
+    printf ("$b <= $b =%d <BR> \n", $a <= $b);
+    printf ("$b == $b =%d <BR> \n", $a == $b);
+    printf ("$b <> $b =%d <BR> \n", $a <> $b);
