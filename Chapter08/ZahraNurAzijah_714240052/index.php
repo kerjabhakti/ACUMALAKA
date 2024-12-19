3b24e6af2e60772c68ebc301786b120d7ca2eecf
zahranur4 <zahranurazijah23@gmail.com>
2024-12-19 16:04:37 +0700

    capek

---
+++ Chapter08/ZahraNurAzijah_714240052/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<<, initial-scale=1.0">
+    <title>HTML Pemula</title>
+</head>
+<body>
+    <h1>Hello <?php echo "Zahra Nurazijah"; ?></h1>
+</body>
+</html>
