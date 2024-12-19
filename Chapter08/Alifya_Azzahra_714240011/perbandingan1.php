c7b9e804b901feb623b4073c12ca9777fadcc178
alifyazz <alifyazz05@gmail.com>
2024-12-19 15:54:04 +0700

    Tugas Alifya chap 8

---
+++ Chapter08/Alifya_Azzahra_714240011/perbandingan1.php
@@ -0,0 +1,10 @@
+<?php
+    $a = 100;
+    $b = 20;
+
+    printf ("$a > $b = %d <BR> \n", $A > $b);
+    printf ("$a >= $b = %d <BR> \n", $A >= $b);
+    printf ("$a < $b = %d <BR> \n", $A < $b);
+    printf ("$a <= $b = %d <BR> \n", $A <= $b);
+    printf ("$a == $b = %d <BR> \n", $A == $b);
+    printf ("$a <> $b = %d <BR> \n", $A <> $b);
