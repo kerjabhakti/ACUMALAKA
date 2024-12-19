b7ff28195e465438d45d5bd87601786e84ce8087
Keylasun <keylasun2018@gmail.com>
2024-12-19 15:42:51 +0700

    Tugas Key

---
+++ Chapter08/Keylasun_714240048/fungsiprint.php
@@ -0,0 +1,4 @@
+<?php$txt = "kampus masa gitu";
+printf("aku belajar pemograman PHP di
+%s<br>, $txt");
+?>
---
+++ Chapter08/Keylasun_714240048/fungsiprintf.php
@@ -0,0 +1,5 @@
+<?php
+$txt = "kampus digital masa gitu";
+printf("aku belajar pemrigraman php di
+%s<br>", $txt);
+?>
