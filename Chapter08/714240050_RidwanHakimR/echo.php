8a37883f697aebc4c04da87b42dfce37f3b15131
911AGENT <ridwanhakimrmdhn@gmail.com>
2024-12-19 15:15:15 +0700

    chapter8

---
+++ Chapter08/714240050_RidwanHakimR/echo.php
@@ -0,0 +1,5 @@
+<?php
+    echo "Hello World <br>";
+    echo "Aku sedang belajar PHP <br>";
+    echo "Ini teks yang dibuat terpisah <br>";
+?>
---
+++ Chapter08/714240050_RidwanHakimR/string.php
@@ -0,0 +1,9 @@
+<?php
+    $nama = "Ridwan Hakim Ramadhan"; //tanda petik satu
+
+    $pesan = "Selamat Datang ($nama)"; //tanda petik dua
+    $pesan2 = 'Selamat Datang ($nama)'; //tanda petik satu
+    # ketika di-echo
+    echo $pesan . '<br>'; #selamat datang (nama seseorang)
+    echo $pesan2 . '<br>'; #Selamat datang ($nama)
+?>
